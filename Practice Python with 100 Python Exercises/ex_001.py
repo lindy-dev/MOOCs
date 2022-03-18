06821d78deee3bcd6f80ab25bd0783a5a3df1755
Shreyas Rao <71974786+lindy-dev@users.noreply.github.com>
2022-03-18 21:02:47 +0530

    Day 1 of 100 python exercises

---
+++ Practice Python with 100 Python Exercises/ex_001.py
@@ -0,0 +1,9 @@
+a = 2
+a = 4
+a = 6
+print(a+a+a)
+
+# Answer is 18 (6+6+6)
+# Python executes code from top to bottom
+# So the value of 6 is the last value assigned
+# to "a" and that gets added three times.
---
+++ Practice Python with 100 Python Exercises/ex_002.py
@@ -0,0 +1,15 @@
+# What is wrong with the following script?
+a = 1
+_a = 2
+_a2 = 3
+# 2a = 4
+
+# Line 5 throws a SyntaxError because variable
+# names cannot start with a number
+# Variable names must start with a letter or an
+# underscore. Everything else will throw a
+# syntax error.
+
+# The underscore prefix is meant as a hint to another
+# programmer that a variable or method is intended for
+# internal use.
